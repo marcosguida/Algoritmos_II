6a0bded2fd1991e4e28e8151ba79403792ac01b0
marcosguida <ribeiroguidam@gmail.com>
2024-09-24 15:35:25 -0300

    EXERCÍCIOS DE PRAGRAMÇÃO - Questão 2 - Situação problema: Adicione um método cumprimentar() à classe Pessoa que imprime uma mensagem de saudação usando o nome. - Data de envio: 24-09-2024
    Close #01

---
+++ src/lista_exercicios/ex2/Main.java
@@ -0,0 +1,19 @@
+package lista_exercicios.ex2;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa("Luiza Souza", 18);
+        Pessoa pessoa2 = new Pessoa("Marcos Paulo", 20);
+        Pessoa pessoa3 = new Pessoa("Carlos", 28);
+
+        System.out.println("Pessoa 1");
+        pessoa1.console();
+        pessoa1.cumprimentar();
+        System.out.println("\nPessoa 2");
+        pessoa2.console();
+        pessoa2.cumprimentar();
+        System.out.println("\nPessoa 3");
+        pessoa3.console();
+        pessoa3.cumprimentar();
+    }
+}
