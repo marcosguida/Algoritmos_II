2163a5d1a82028a9c7f03c71765c76a3e7cd4e6c
marcosguida <ribeiroguidam@gmail.com>
2024-09-24 15:16:55 -0300

    EXERCÍCIOS DE PRAGRAMÇÃO - Questão 1 - Situação problema: Crie uma classe Pessoa com os atributos: nome e idade. Crie um objeto dessa classe, atribua valores aos atributos e imprima-os no console. - Data de envio: 24-09-2024

---
+++ src/lista_exercicios/ex1/Main.java
@@ -0,0 +1,16 @@
+package lista_exercicios.ex1;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa("Luiza Souza", 18);
+        Pessoa pessoa2 = new Pessoa("Marcos Paulo", 20);
+        Pessoa pessoa3 = new Pessoa("Carlos", 28);
+
+        System.out.println("Pessoa 1");
+        pessoa1.console();
+        System.out.println("\nPessoa 2");
+        pessoa2.console();
+        System.out.println("\nPessoa 3");
+        pessoa3.console();
+    }
+}
