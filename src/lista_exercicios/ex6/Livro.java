2082e7b3e7e4186e46b8e77ef2fcbe5876efe0b2
marcosguida <ribeiroguidam@gmail.com>
2024-09-26 22:40:49 -0300

    EXERCÍCIOS DE PRAGRAMÇÃO - Questão 6 - Situação problema: Crie uma classe Biblioteca que contém uma lista de Livro e métodos para adicionar e remover livros. - Data de envio: 26-09-2024
    Close #06

---
+++ src/lista_exercicios/ex6/Livro.java
@@ -0,0 +1,58 @@
+package lista_exercicios.ex6;
+
+public class Livro {
+
+    private String titulo, autor;
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Ano de Publicação: " + anoPublicacao);
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public static void main(String[] args) {
+        Livro livro = new Livro("O Poder do Hábito", "Charles Duhigg", 2012);
+        Livro livro1 = new Livro("Os princípios da filosofia de Santo Tomás de Aquino", "O.p. Fr. Édouard Hugon", 2023);
+        Livro livro2 = new Livro("Pequena via de Santa Teresinha: Caminho de Cura e Santidade", "r André L. Botelho de Andrade ", 2021);
+
+        livro.exibirDetalhes();
+        System.out.println("\n");
+
+        livro1.exibirDetalhes();
+        System.out.println("\n");
+
+        livro2.exibirDetalhes();
+        System.out.println("\n");
+    }
+}
