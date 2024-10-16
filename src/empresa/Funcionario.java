7194966ee9b90acafa8c6a962efe721ca42ff1fe
marcosguida <ribeiroguidam@gmail.com>
2024-10-16 13:45:13 -0300

    EXERCÍCIOS DE PRAGRAMÇÃO - Questao 11 - Situação problema: Faça um programa para um jogo de RPG onde um personagem poderá escolher entre as classes: Mago, Guerreiro, Sacerdote e Ladrão. O Mago poderá castar magias arcanas. O sacerdote fazer milagres (magias divinas). O ladrão poderá ﬁcar invisível. E o guerreiro poderá usar armaduras pesadas. Todos possuem HP (vida) e SP (alma). - Data de envio: 16-10-2024
    Close #11

---
+++ src/empresa/Funcionario.java
@@ -0,0 +1,37 @@
+package empresa;
+
+public class Funcionario {
+    private String nome;
+    private String cpf;
+    private String cargo;
+
+    public Funcionario(String nome, String cpf , String cargo) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.cargo = cargo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
