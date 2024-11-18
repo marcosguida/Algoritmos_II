aa3edfa6bf116024a399945d2a5bbe46965b8580
Marcos Guida <105091587+marcosguida@users.noreply.github.com>
2024-11-17 22:34:38 -0300

    Update Carrinho.java

--- src/conta_bancaria/Carrinho.java
+++ src/conta_bancaria/Carrinho.java
@@ -27,15 +27,15 @@ class Carrinho {
 
     public static void main(String[] args) {
 
-        Alimento leite = new Alimento("Leite", 2.99, LocalDate.of(2023, 10, 15));
-        Livro livro = new Livro("Dom Casmurro", 34.90);
-        Eletronicos foneDeOuvido = new Eletronicos("Fone de Ouvido", 149.90);
+        Alimento leite = new Alimento("Energetico Power", 10.49, LocalDate.of(2024, 10, 15));
+        Livro livro = new Livro("O processo", 84.90);
+        Eletronicos foneDeOuvido = new Eletronicos("Fone de Ouvido TWS JBL", 69.90);
 
         Carrinho carrinho = new Carrinho();
 
-        carrinho.addItem(leite, 2);
-        carrinho.addItem(livro, 1);
-        carrinho.addItem(foneDeOuvido, 1);
+        carrinho.addItem(2);
+        carrinho.addItem(1);
+        carrinho.addItem(1);
 
         for (Item item : carrinho.produtos) {
             System.out.println(item.nome);
