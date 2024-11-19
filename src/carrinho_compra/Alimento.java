37698457b62884d297ee13eae91f6f71e69cd4ba
marcosguida <ribeiroguidam@gmail.com>
2024-11-19 14:49:36 -0300

    Reorganização de código

--- src/carrinho_compra/Alimento.java
+++ src/carrinho_compra/Alimento.java
@@ -1,11 +1,9 @@
 package carrinho_compra;
 
-import java.time.LocalDate;
-
 public class Alimento extends Item {
     private String origem;
 
-    public Alimento(String nome, double preco, LocalDate origem) {
+    public Alimento(String nome, double preco, String origem) {
         super(nome, preco);
         this.origem = origem;
     }
