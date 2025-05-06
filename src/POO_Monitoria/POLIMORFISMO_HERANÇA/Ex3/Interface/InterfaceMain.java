f75683136f9961988cbc7fdc85a5cd1633ac09c8
Marcos Guida <105091587+marcosguida@users.noreply.github.com>
2025-05-06 09:07:19 -0300

    Update InterfaceMain.java

--- src/POO_Monitoria/POLIMORFISMO_HERANÇA/Ex3/Interface/InterfaceMain.java
+++ src/POO_Monitoria/POLIMORFISMO_HERANÇA/Ex3/Interface/InterfaceMain.java
@@ -1,10 +1,9 @@
 package POO_Monitoria.POLIMORFISMO_HERANÇA.Ex3.Interface;
 
-// Interface
 interface Pagavel {
     double calcularPagamento();
     
-    // Método default (a partir do Java 8)
+    // Método default 
     default void exibirPagamento() {
         System.out.println("Pagamento: R$" + calcularPagamento());
     }
@@ -44,7 +43,7 @@ public class InterfaceMain {
         Pagavel funcionario = new Funcionario(3000);
         Pagavel fatura = new Fatura(1000, 0.1);
         
-        funcionario.exibirPagamento(); // Pagamento: R$3000.0
-        fatura.exibirPagamento(); // Pagamento: R$900.0
+        funcionario.exibirPagamento(); 
+        fatura.exibirPagamento(); 
     }
 }
