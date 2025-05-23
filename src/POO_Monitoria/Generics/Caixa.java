a7f0b4c52f973a41f8506962fb845aebeac38d52
marcosguida <ribeiroguidam@gmail.com>
2025-05-23 19:13:51 -0300

    Generics em Java

---
+++ src/POO_Monitoria/Generics/Caixa.java
@@ -0,0 +1,25 @@
+package Generics;
+
+public class Caixa<T> {
+    private T conteudo;
+    
+    public void colocar(T item) {
+        this.conteudo = item;
+    }
+    
+    public T retirar() {
+        return conteudo;
+    }
+
+    public static void main(String[] args) {
+        Caixa<String> caixaTexto = new Caixa<>();
+        caixaTexto.colocar("Hello");
+
+        Caixa<Integer> caixaNumero = new Caixa<>();
+        caixaNumero.colocar(42);
+
+        System.out.println("\n");
+        System.out.println("Conteúdo da caixa de texto: " + caixaTexto.retirar());
+        System.out.println("Conteúdo da caixa de número: " + caixaNumero.retirar());
+    }
+}
