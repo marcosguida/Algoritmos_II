e9d370e2a469833b900d2ef4deebe7c4035307b6
marcosguida <ribeiroguidam@gmail.com>
2025-05-26 14:47:54 -0300

    Update

--- src/POO_Monitoria/Collections/Collectors/AgrupadorPalavras.java
+++ src/POO_Monitoria/Collections/Collectors/AgrupadorPalavras.java
@@ -1,6 +1,5 @@
-package Collections;
+package Collections.Collectors;
 
-// AgrupadorPalavras.java
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
--- src/POO_Monitoria/Collections/Collectors/CollectorProduto.java
+++ src/POO_Monitoria/Collections/Collectors/CollectorProduto.java
@@ -1,4 +1,4 @@
-package Collections;
+package Collections.Collectors;
 
 import java.util.*;
 import java.util.stream.Collectors;
