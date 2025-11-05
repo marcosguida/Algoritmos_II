c4df34fc3f0254fa17344fdd8547e6ed41b18fe3
marcosguida <ribeiroguidam@gmail.com>
2025-11-05 19:57:22 -0300

    Update in Java Code

--- src/POO_Monitoria/Generics/Lista.java
+++ src/POO_Monitoria/Generics/Lista.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 public class Lista {
 
     public static void main(String[] args) {
-        
+        // Sem generics
         // Com generics
         List<String> lista = new ArrayList<>();
-        lista.add("CPF");
+        lista.add("041456767");
         String cpfv = lista.get(0);
         System.out.println(cpfv);
     }
